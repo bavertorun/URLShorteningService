22b3c5860dcf5c344248355d999a7866385f434b
Baver <bavertorunx@gmail.com>
2024-08-08 20:25:59 +0300

    feat: Add dotenv and mongodb
    -Dotenv imported to the project.
    -Mongodb was imported to the project and connection was made.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ShortenModule } from './shorten/shorten.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [ShortenModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    ShortenModule,
+  ],
   controllers: [],
   providers: [],
 })
